b27fb88dac4cc287d9cfa0946a0fb9020eaff8c5
Darlan <darlandc@gmail.com>
2020-12-10 20:40:28 -0300

    update service

--- projects/skyrim-planning/src/lib/services/tasks.service.ts
+++ projects/skyrim-planning/src/lib/services/tasks.service.ts
@@ -1,3 +1,4 @@
+import { Task } from './../tasks/task.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,4 +9,47 @@ export class TasksService {
   cache: {};
 
   constructor() { }
+
+  listAll(): Task[] {
+    const Tasks = localStorage.Tasks;
+    return Tasks ? JSON.parse(Tasks) : [];
+  }
+
+   register(Task: Task): void {
+     const Tasks = this.listAll();
+     Task.id = new Date().getTime();
+     Tasks.push(Task);
+     localStorage.Tasks = JSON.stringify(Tasks);
+   }
+
+  findById(id: number): Task {
+    const Tasks: Task[] = this.listAll();
+    return Tasks.find(Task => Task.id === id);
+  }
+
+  atualizar(Task: Task): void {
+    const Tasks: Task[] = this.listAll();
+    Tasks.forEach((obj, index, objs) => {
+      if (Task.id === obj.id ) {
+          objs[index] = Task;
+      }
+      localStorage.Tasks = JSON.stringify(Tasks);
+    });
+  }
+
+  remove(id: number): void {
+    let Tasks: Task[] = this.listAll();
+    Tasks = Tasks.filter(Task => Task.id !== id);
+    localStorage.Tasks = JSON.stringify(Tasks);
+  }
+
+  changeStatus(id: number): void {
+    const Tasks: Task[] = this.listAll();
+    Tasks.forEach((obj, index, objs) =>  {
+      if (id === obj.id) {
+        objs[index].done = !obj.done;
+      }
+    });
+    localStorage.Tasks = JSON.stringify(Tasks);
+  }
 }
